26612199553d9968557428259235bce25ad06d93
Anton27 <antonkleymenov27@yandex.ru>
2020-10-14 15:00:49 +0300

    bug fixes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-  $('.burger').click(function (event) {
-      $('.burger,.header__menu').toggleClass('active');
-  });
+document.querySelector('.burger, .header__menu').addEventListener('click', () => {
+  document.querySelector('.burger').classList.toggle('active');
+  document.querySelector('.header__menu').classList.toggle('active');
+
 });
